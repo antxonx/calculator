191c2c51791587ec8975f951399178bf07f20e4a
alejandro-antonio <alejandro@pcpartners.com.mx>
2020-11-25 10:39:27 -0600

    different files

---
+++ include/myFrame.h
@@ -0,0 +1,15 @@
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame();
+
+private:
+    void OnHello(wxCommandEvent &event);
+    void OnExit(wxCommandEvent &event);
+    void OnAbout(wxCommandEvent &event);
+};
