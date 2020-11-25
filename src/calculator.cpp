91af7f26c448cc81dcb6b5642f4bced59fee6943
alejandro-antonio <alejandro@pcpartners.com.mx>
2020-11-25 13:57:23 -0600

    screen append

--- src/calculator.cpp
+++ src/calculator.cpp
@@ -2,15 +2,18 @@
 
 Calculator::Calculator(wxSize size) : wxFrame(NULL, wxID_ANY, "Calculator", wxDefaultPosition, size)
 {
-    unsigned int i = 0;
-    this->Bind(wxEVT_MENU, &Calculator::onExit, this, wxID_EXIT);
+    //Font size
     wxFont font = wxFont(wxFontInfo(20));
+    //Container
     box = new wxBoxSizer(wxVERTICAL);
+    //Display element
     display = new wxTextCtrl(this, wxID_ANY, wxT(""), wxDefaultPosition, wxDefaultSize, wxTE_RIGHT);
     display->SetFont(font);
+    //Add displey to container
     box->Add(display, 0, wxEXPAND | wxTOP | wxBOTTOM, 10);
+    //Grid element for buttons
     grid = new wxGridSizer(5, 4, 3, 3);
-
+    //Buttons definitions
     grid->Add(new wxStaticText(this, wxID_ANY, wxT("")), 0, wxEXPAND);
     grid->Add(new wxStaticText(this, wxID_ANY, wxT("")), 0, wxEXPAND);
     grid->Add(new wxButton(this, CALCULATOR::CALC_CLS, wxT("CLS")), 0, wxEXPAND);
@@ -31,12 +34,14 @@ Calculator::Calculator(wxSize size) : wxFrame(NULL, wxID_ANY, "Calculator", wxDe
     grid->Add(new wxButton(this, CALCULATOR::CALC_POINT, wxT(".")), 0, wxEXPAND);
     grid->Add(new wxButton(this, CALCULATOR::CALC_EQUAL, wxT("=")), 0, wxEXPAND);
     grid->Add(new wxButton(this, CALCULATOR::CALC_PLUS, wxT("+")), 0, wxEXPAND);
+    //Add buttons grid to container
     box->Add(grid, 1, wxEXPAND);
     SetSizer(box);
     SetMinSize(size);
     Centre();
-
-    for(const auto OPT : CALCULATOR::All){
+    //Handle events
+    this->Bind(wxEVT_MENU, &Calculator::onExit, this, wxID_EXIT);
+    for(const CALCULATOR::OPTIONS OPT : CALCULATOR::All){
         this->Bind(wxEVT_BUTTON, &Calculator::onClick, this, OPT);
     }
 }
@@ -49,5 +54,34 @@ void Calculator::onExit(wxCommandEvent &event)
 void Calculator::onClick(wxCommandEvent &event) 
 {
     wxButton *button = wxDynamicCast(event.GetEventObject(), wxButton);
-    this->display->SetLabelText(button->GetLabel());
+    switch (button->GetId())
+    {
+    case CALCULATOR::CALC_CLS:
+        /* code */
+        break;
+    case CALCULATOR::CALC_BACK:
+        /* code */
+        break;
+    case CALCULATOR::CALC_DIV:
+        /* code */
+        break;
+    case CALCULATOR::CALC_TIMES:
+        /* code */
+        break;
+    case CALCULATOR::CALC_MINUS:
+        /* code */
+        break;
+    case CALCULATOR::CALC_POINT:
+        /* code */
+        break;
+    case CALCULATOR::CALC_EQUAL:
+        /* code */
+        break;
+    case CALCULATOR::CALC_PLUS:
+        /* code */
+        break;
+    default:
+        this->display->SetLabelText(this->display->GetLabel() + button->GetLabel());
+        break;
+    }
 }
