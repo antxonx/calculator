1a4b78980d65811be77e0c66c78751e9f9b2dee7
alejandro-antonio <alejandro@pcpartners.com.mx>
2020-11-26 10:29:02 -0600

    exception principal

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include <main.h>
 #include <calculator.h>
+#include <exception>
 
 wxIMPLEMENT_APP(MyApp);
 
 bool MyApp::OnInit()
 {
-    Calculator::CalculatorWindow *calculator = new Calculator::CalculatorWindow(wxSize(400, 500));
-    calculator->Show(true);
-    SetTopWindow(calculator);
+    try {
+        Calculator::CalculatorWindow *calculator = new Calculator::CalculatorWindow(wxSize(400, 500));
+        calculator->Show(true);
+        SetTopWindow(calculator);
+    } catch(const std::exception &e) {
+        wxMessageDialog errDialog(NULL, e.what(), "Error", wxOK);
+        errDialog.ShowModal();
+    }
     return true;
 }
