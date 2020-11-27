d0966cb86ea0d1afbc9a0c387ff9825171bdf1f0
Antxony <dantonyofcarim@gmail.com>
2020-11-27 10:56:49 -0600

    icon

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <main.hh>
 #include <calculator.hh>
 #include <exception>
+#include <cstdio>
+#include <unistd.h>
+#include <iostream>
+#define GetCurrentDir getcwd
 
 wxIMPLEMENT_APP(MyApp);
 
@@ -8,8 +12,12 @@ bool MyApp::OnInit()
 {
     try
     {
+        char buff[FILENAME_MAX];
+        GetCurrentDir( buff, FILENAME_MAX );
+        std::string current_working_dir(buff);
         Calculator::CalculatorWindow *calculator = new Calculator::CalculatorWindow(wxSize(400, 500));
         calculator->Show(true);
+        calculator->SetIcon(wxIcon(current_working_dir + "/assets/favicon.ico"));
         SetTopWindow(calculator);
     }
     catch (const std::exception &e)
