b2b5d9ec93615e84b4706ebed6f5bb0bb2ccbff6
Antxony <dantonyofcarim@gmail.com>
2020-11-27 09:47:29 -0600

    arith.so with add op

---
+++ lib/calcmath/arith.s
@@ -0,0 +1,23 @@
+section .data
+section .bss
+operand1: resq 1
+operand2: resq 1
+tmp: resq 1
+section .text
+global add:function
+add:
+    push rbp
+    mov rbp, rsp
+
+    movsd [rel operand1], xmm0
+    movsd [rel operand2], xmm1
+    fld qword[rel operand1]
+    fld qword[rel operand2]
+
+    fadd
+
+    fstp qword[rel tmp]
+    movsd xmm0, [rel tmp]
+
+    leave
+    ret
